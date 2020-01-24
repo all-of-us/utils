448e1c0fc188da521bf1c2c22875b27b7080d4a8
CH Albach <calbach@google.com>
2020-01-24 15:52:36 -0800

    yield self

--- serviceaccounts.rb
+++ serviceaccounts.rb
@@ -9,7 +9,7 @@ require_relative "utils/common"
 # The test SA key is a special case for local development, and is initialized
 # but not deleted.
 class ServiceAccountContext
-  attr_reader :service_account
+  attr_reader :project, :service_account, :keyfile_path
 
   SERVICE_ACCOUNT_KEY_PATH = "sa-key.json"
 
@@ -43,7 +43,7 @@ class ServiceAccountContext
       # Don't generate another key if this account is already active. This can
       # happen for nested service account contexts, for example.
       common.status "Attaching to existing keyfile @ #{@keyfile_path}"
-      yield
+      yield(self)
       return
     end
 
@@ -51,13 +51,13 @@ class ServiceAccountContext
       common.status "Copying key from GCS for #{@service_account} @ #{@keyfile_path}"
       common.run_inline %W{gsutil cp gs://#{@project}-credentials/app-engine-default-sa.json
             #{@keyfile_path}}
-      yield
+      yield(self)
     else
       common.status "Creating new key for #{@service_account} @ #{@keyfile_path}"
       common.run_inline %W{gcloud iam service-accounts keys create #{@keyfile_path}
           --iam-account=#{@service_account} --project=#{@project}}
       begin
-        yield
+        yield(self)
       ensure
         tmp_private_key = `grep private_key_id #{@keyfile_path} | cut -d\\\" -f4`.strip()
         common.run_inline %W{gcloud iam service-accounts keys delete #{tmp_private_key} -q
